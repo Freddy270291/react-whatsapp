40b0d2c66f2dbfc9cb7586f182665799fc3f47d1
Federico Giovannini <giovannini.federico@hotmail.it>
2021-03-21 19:55:20 +0100

    Sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,44 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+        <h1>Sidebar Chat</h1>
+      </div>
     </div>
   );
 }
