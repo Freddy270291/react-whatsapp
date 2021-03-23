13735ae9e574422e1ce7d8947e06ae0c7575f272
Federico Giovannini <giovannini.federico@hotmail.it>
2021-03-23 19:28:23 +0100

    main chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,12 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import "./Chat.css";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
+  const [input, setInput] = useState("");
   const [seed, setSeed] = useState("");
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("You typed", input);
+    setInput("");
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -28,12 +38,31 @@ function Chat() {
           </IconButton>
         </div>
       </div>
-      <div className="chat__body"></div>
-      <div className="chat__footer"></div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">Federico Giovannini</span>
+          Hey Guys
+          <span className="chat__timestamp">7:51pm</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message.."
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
 
 export default Chat;
-
-//1:31:00 ChatBody
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBZlXFvKI3I0xRBMaCWhKbJZcrUphXrGI8",
   authDomain: "whatsapp-clone-d7ed0.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:222614066303:web:810901c7ad4b7d8f9f5b4a",
   measurementId: "G-169M2T3D4C",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
